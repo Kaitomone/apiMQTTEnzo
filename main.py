1ea9e97c7b507a62c9c965da4297683c64d04eb8
Enzo Richard <enzo.richard.pro2@gmail.com>
2023-04-06 10:13:46 -0400

    Test envoie de message

--- main.py
+++ main.py
@@ -8,33 +8,6 @@ mqtt_config = MQTTConfig(host="172.16.5.101", port=1883)
 mqtt = FastMQTT(config=mqtt_config)
 mqtt.init_app(app)
 
-@mqtt.on_connect()
-def connect(client, flags, rc, properties):
-    mqtt.client.subscribe("enzo/led/couleur/R") #subscribing mqtt topic
-    print("Connected: ", client, flags, rc, properties)
-
-@mqtt.subscribe("enzo/led/couleur/R")
-async def message_to_topic(client, topic, payload, qos, properties):
-    print("Received message to specific topic: ", topic, payload.decode())
-
-@mqtt.on_connect()
-def connect(client, flags, rc, properties):
-    mqtt.client.subscribe("enzo/led/couleur/G")  # subscribing mqtt topic
-    print("Connected: ", client, flags, rc, properties)
-
-@mqtt.subscribe("enzo/led/couleur/G")
-async def message_to_topic(client, topic, payload, qos, properties):
-    print("Received message to specific topic: ", topic, payload.decode())
-
-@mqtt.on_connect()
-def connect(client, flags, rc, properties):
-    mqtt.client.subscribe("enzo/led/couleur/B")  # subscribing mqtt topic
-    print("Connected: ", client, flags, rc, properties)
-
-@mqtt.subscribe("enzo/led/couleur/B")
-async def message_to_topic(client, topic, payload, qos, properties):
-    print("Received message to specific topic: ", topic, payload.decode())
-    
 @mqtt.on_disconnect()
 def disconnect(client, packet, exc=None):
     print("Disconnected")
@@ -43,8 +16,12 @@ def disconnect(client, packet, exc=None):
 def subscribe(client, mid, qos, properties):
     print("subscribed", client, mid, qos, properties)
 
+#Publish la couleur sur l'ESP32 en dur
 @app.get("/test")
 async def func():
-    mqtt.publish("enzo/led/couleur", "127 0 122") #publishing mqtt topic
+    mqtt.publish("enzo/led/couleur", "255 0 0 test") #publishing mqtt topic
 
     return {"result": True,"message":"Published" }
+
+
+
